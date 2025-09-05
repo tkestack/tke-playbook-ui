0df02c1a125d2543f459a9dbe7b39d3ab2351f83
tangtang <841453272@qq.com>
2025-09-05 11:53:16 +0800

    修改hash路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 // 导入页面组件
 const Home = () => import('../view/Home.vue')
@@ -15,7 +15,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
